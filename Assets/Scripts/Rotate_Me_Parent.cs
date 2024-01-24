0813e7137bc2975eb9a5997c62b47fe5a77e5b52
nickreno <nick.p.reno@gmail.com>
2024-01-24 15:11:12 -0500

    Rotation
    I created a script that allows the a game object to rotate upon input. The 3 objects that are listed as children rotate accordingly, keeping their local orientation. This can be useful for switching between the stations with each station's children on them as well.
    The previous storage error was solved by restarting Unity.

--- Assets/Scripts/Rotate_Me_Parent.cs
+++ Assets/Scripts/Rotate_Me_Parent.cs
@@ -4,30 +4,72 @@ using UnityEngine;
 
 public class Rotate_Me_Parent : MonoBehaviour
 {
+    //ROTATE VALUES:    0   120 240 
 
-    [SerializeField]
-    private int rotValue;
 
-    private Quaternion _targetRot;
+    private bool rotating = false;
+    public GameObject objectToRotate;
 
-    private void Start()
+    //Station Rotation Values
+    private Quaternion rotation1 = Quaternion.Euler(new Vector3(0, 0, 0));
+    private Quaternion rotation2 = Quaternion.Euler(new Vector3(0, 120, 0));
+    private Quaternion rotation3 = Quaternion.Euler(new Vector3(0, 240, 0));
+
+    public int moveValue = 0;
+
+    private void Update()
     {
-        _targetRot = this.transform.rotation;
-    }
-    void Update()
-    {
-        
-        switch (rotValue)
+        if (moveValue == 1)
         {
-            case 0:
-                _targetRot = Quaternion.AngleAxis(-90, transform.forward); 
-                break;
-
-            case 1:
-                _targetRot = Quaternion.AngleAxis(90, transform.forward);
-                break;
+            StartCoroutine(rotateObject(objectToRotate, rotation1, 1f));
         }
+        else if (moveValue == 2)
+        {
+            StartCoroutine(rotateObject(objectToRotate, rotation2, 1f));
+        }
+        else if(moveValue == 3)
+        {
+            StartCoroutine(rotateObject(objectToRotate, rotation3, 1f));
+        }
+        else
+        {
+            //do nothing. Do not rotate
+        }
+    }
 
 
+    IEnumerator rotateObject(GameObject gameObjectToMove, Quaternion newRot, float duration)
+    {
+        if (rotating)
+        {
+            yield break;
+        }
+        rotating = true;
+
+        Quaternion currentRot = gameObjectToMove.transform.rotation;
+
+        float counter = 0;
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            gameObjectToMove.transform.rotation = Quaternion.Lerp(currentRot, newRot, counter / duration);
+            yield return null;
+        }
+        rotating = false;
     }
 }
+
+
+
+/*
+this.transform.rotation = Quaternion.Slerp(from.rotation, Station2Quat, timeCount * 0.01f); // * 0.01f
+timeCount = (timeCount * 0.01f) + Time.deltaTime; 
+
+^this breaks^ with the 0.01f
+
+*/
+
+
+//Script Credits:
+//  Rotate GameObject over time
+//  https://stackoverflow.com/questions/37586407/rotate-gameobject-over-time
