8e88e13d2131566730c2178e55374822f1914f69
nickreno <nick.p.reno@gmail.com>
2024-02-14 16:25:34 -0500

    Updated drone fall

--- Assets/Scripts/DroneDirection_KEYBOARD.cs
+++ Assets/Scripts/DroneDirection_KEYBOARD.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DroneDirection_KEYBOARD : MonoBehaviour
 {
     Rigidbody rb;
+
+    private Vector3 kickForce = new Vector3(0.5f, 0, 0);
 
     private Vector3 force1x = new Vector3(1, 0, 0);
     private Vector3 force2x = new Vector3(-1, 0, 0);
@@ -15,6 +18,8 @@ public class DroneDirection_KEYBOARD : MonoBehaviour
     private Vector3 moveZaxis = new Vector3(0, 0, 5);
     private Vector3 moveXaxis = new Vector3(5, 0, 0);
 
+    private bool isFalling = false;
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
@@ -22,25 +27,54 @@ public class DroneDirection_KEYBOARD : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //rb.AddForce(force3x, ForceMode.Impulse);
-            this.transform.position += moveZaxis;
+            isFalling = true;
+            rb.useGravity = true;
+            StartCoroutine(randomFlightEvent());
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        if (isFalling == true)
         {
-            //rb.AddForce(force4x, ForceMode.Impulse);
-            this.transform.position -= moveZaxis;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                //rb.AddForce(force3x, ForceMode.Impulse);
+                this.transform.position += moveZaxis;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                //rb.AddForce(force4x, ForceMode.Impulse);
+                this.transform.position -= moveZaxis;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                //rb.AddForce(force2x, ForceMode.Impulse);
+                this.transform.position -= moveXaxis;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                //rb.AddForce(force1x, ForceMode.Impulse);
+                this.transform.position += moveXaxis;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isFalling = false;
+    }
+
+
+    IEnumerator randomFlightEvent()
+    {
+        yield return new WaitForSeconds(5);
+        if (isFalling == true) 
         {
-            //rb.AddForce(force2x, ForceMode.Impulse);
-            this.transform.position -= moveXaxis;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+            this.transform.position += moveXaxis * 2;
+            StartCoroutine(randomFlightEvent());
+        } else
         {
-            //rb.AddForce(force1x, ForceMode.Impulse);
-            this.transform.position += moveXaxis;
+            yield return null;
         }
     }
 }
