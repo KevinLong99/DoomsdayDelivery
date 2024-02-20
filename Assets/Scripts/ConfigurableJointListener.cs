4203db7f334f35c313286ecd2aa46289c2fe8d64
nickreno <nick.p.reno@gmail.com>
2024-02-20 16:05:03 -0500

    fixed lever pulldown positioning and player orientation

--- Assets/Scripts/ConfigurableJointListener.cs
+++ Assets/Scripts/ConfigurableJointListener.cs
@@ -16,23 +16,32 @@ public class ConfigurableJointListener : MonoBehaviour
 
     public Spawner mySpawner;
 
+    float currentPosition, lowerLimit, upperLimit;
+
+
     void Start()
     {
         configurableJoint = GetComponent<ConfigurableJoint>();
+        currentPosition = configurableJoint.transform.localPosition.y;
+        lowerLimit = configurableJoint.linearLimit.limit * -1; // Assuming symmetric limits
+        upperLimit = configurableJoint.linearLimit.limit;
     }
 
     void FixedUpdate()
     {
         // Assuming you're using the joint's linear limit for the y-axis
-        float currentPosition = configurableJoint.transform.localPosition.y;
-        float lowerLimit = configurableJoint.linearLimit.limit * -1; // Assuming symmetric limits
-        float upperLimit = configurableJoint.linearLimit.limit;
+        
 
         // Reached Lower Limit
         if (Mathf.Abs(currentPosition - lowerLimit) < positionThreshold)
         {
             if (jointLimitState != JointLimitState.Lower)
+            {
+                //this happens upon start and ONLY on start.
+                Debug.Log("lowerlimitINVOKE");
                 OnLowerLimitReached.Invoke();
+            }
+                
 
             jointLimitState = JointLimitState.Lower;
         }
@@ -40,7 +49,11 @@ public class ConfigurableJointListener : MonoBehaviour
         else if (Mathf.Abs(currentPosition - upperLimit) < positionThreshold)
         {
             if (jointLimitState != JointLimitState.Upper)
+            {
+                Debug.Log("upperlimitINVOKE");
                 OnUpperLimitReached.Invoke();
+            }
+                
 
             jointLimitState = JointLimitState.Upper;
         }
@@ -54,11 +67,13 @@ public class ConfigurableJointListener : MonoBehaviour
     // Call these methods if you want to manually trigger the events
     public void TriggerLowerLimitReached()
     {
+        Debug.Log("lower");
         OnLowerLimitReached.Invoke();
     }
 
     public void TriggerUpperLimitReached()
     {
+        Debug.Log("upper");
         OnUpperLimitReached.Invoke();
     }
 
