df15badd55ef3ca9f78cb78293b71ba2a3d368ac
rachel-west <rachel4zoe@gmail.com>
2024-03-02 16:38:10 -0500

    Rachel Prototype Sphere almost done

---
+++ Assets/BuildingRenderClip.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class BuildingRenderClip : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Building")
+        {
+            Debug.Log(other.tag);
+            Transform[] allChildren = other.GetComponentsInChildren<Transform>(true);
+            foreach (Transform child in allChildren)
+            {
+                if (child.tag != "Building")
+                    child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Building")
+        {
+            Transform[] allChildren = other.GetComponentsInChildren<Transform>(true);
+            foreach (Transform child in allChildren)
+            {
+                if (child.tag != "Building")
+                    child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
