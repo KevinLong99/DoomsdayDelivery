e8b80862c2993d624bccf27443c4fb1fae5ea380
KevinLong99 <74923205+KevinLong99@users.noreply.github.com>
2024-03-20 14:51:46 -0400

    TutorialManager update

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TutorialManager : MonoBehaviour
+{
+    // Public list visible in the inspector
+    public List<GameObject> Steps;
+
+    // Function to remove (delete) the game object at the specified index
+    public void RemoveStep(int number)
+    {
+        if (number >= 0 && number < Steps.Count)
+        {
+            Destroy(Steps[number]);
+            Steps.RemoveAt(number);
+        }
+        else
+        {
+            Debug.LogError("Index out of range: " + number);
+        }
+    }
+
+    // Function to enable the game object at the specified index
+    public void AddStep(int number)
+    {
+        if (number >= 0 && number < Steps.Count)
+        {
+            Steps[number].SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Index out of range: " + number);
+        }
+    }
+}
