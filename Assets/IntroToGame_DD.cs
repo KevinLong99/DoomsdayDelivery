6eed559802d3da15993d24a95793e8df60ea2b36
nickreno <nick.p.reno@gmail.com>
2024-04-16 12:28:02 -0400

    intro sequence almost set
    johnny attempting masking (god help us)

--- Assets/IntroToGame_DD.cs
+++ Assets/IntroToGame_DD.cs
@@ -16,6 +16,14 @@ public class IntroToGame_DD : MonoBehaviour
 
     private Vector3 camEndPos = new Vector3(0, -10f, 2f);
 
+    public Typewriter_UI typeWriteScript;
+
+    private string inTheYearTxt = "In the city of [REDACTED] in the year 2084...\r\n\r\n" +
+        "The war has let up enough for aid to be delivered to survivors in desperate need...";
+
+    private float counter, timeDur = 0;
+
+    public Light spotLightThruster;
 
     void Start()
     {
@@ -30,6 +38,17 @@ public class IntroToGame_DD : MonoBehaviour
     private IEnumerator BeginGameCoroutine()
     {
         //lights flicker on 
+        counter = 0;
+        timeDur = 3;
+
+        while (counter < timeDur)
+        {
+            RenderSettings.ambientIntensity = Mathf.Lerp(0.1f, 0.75f, counter / timeDur);
+            RenderSettings.reflectionIntensity = Mathf.Lerp(0.1f, 0.75f, counter / timeDur);
+            spotLightThruster.intensity = Mathf.Lerp(1, 0, counter/timeDur);
+            counter += Time.deltaTime;
+            yield return null;
+        }
 
         yield return new WaitForSeconds(2);
 
@@ -39,8 +58,8 @@ public class IntroToGame_DD : MonoBehaviour
         yield return new WaitForSeconds(1);
 
         //ship takes off, revealing the skyline
-        float counter = 0;
-        float timeDur = 7;
+        counter = 0;
+        timeDur = 1;  //FIXME change to 7
 
         Vector3 posStart = cameraToMove.transform.position;
         while (counter < timeDur)
@@ -53,18 +72,32 @@ public class IntroToGame_DD : MonoBehaviour
         yield return new WaitForSeconds(1);
 
         //fade lights out, including the one over the thruster
-        RenderSettings.ambientIntensity = 0.25f;
-        RenderSettings.reflectionIntensity = 0.25f;
-        yield return new WaitForSeconds(1);
+        counter = 0;
+        timeDur = 3;
+
+        while (counter < timeDur)
+        {
+            RenderSettings.ambientIntensity = Mathf.Lerp(0.75f, 0.25f, counter/timeDur);
+            RenderSettings.reflectionIntensity = Mathf.Lerp(0.75f, 0.25f, counter / timeDur);
+            
+            counter += Time.deltaTime;
+            yield return null;
+        }
+        //RenderSettings.ambientIntensity = 0.25f;
+        //RenderSettings.reflectionIntensity = 0.25f;
+        //startGameText.SetActive(true);
 
         //black screen with white text appears:
         inTheYearText.SetActive(true);
+        typeWriteScript.StartTypewriterView(inTheYearTxt);
 
         yield return new WaitForSeconds(3);
+
 
         //switch to game scene 
         //sceneManagerScript.BeginGame();
         //fadeScreenScript.FadeOut();
+        //startGameText.SetActive(false);
     }
      
 }
