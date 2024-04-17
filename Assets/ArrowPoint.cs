b72b1bcddd2e22f2a100dc85984013c566f58b71
KevinLong99 <74923205+KevinLong99@users.noreply.github.com>
2024-04-17 12:41:15 -0400

    Drone arrow function fully implemented, made changes to ArrowParent.cs

--- Assets/ArrowPoint.cs
+++ Assets/ArrowPoint.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class ArrowPoint : MonoBehaviour
 {
-    public GameObject target;
+    public ProgressManager myProgressManager; // Reference to the ProgressManager GameObject
+    public int myCurrentMedTent; // Stores the current Med Tent number
+    public List<GameObject> MedTentList; // List of Med Tent GameObjects
+    public GameObject target; // Current target GameObject
 
-    // Update is called once per frame
     void Update()
     {
-        // Determine which direction to rotate towards
-        Vector3 targetDirection = target.transform.position - transform.position;
+        // Update myCurrentMedTent to match the value from ProgressManager
+        if (myProgressManager != null)
+        {
+            myCurrentMedTent = myProgressManager.CurrentMedTent;
+        }
 
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, 2 * Mathf.PI, 0.0f);
+        // Assign the target based on myCurrentMedTent value
+        if (MedTentList != null && MedTentList.Count > 0)
+        {
+            switch (myCurrentMedTent)
+            {
+                case 1:
+                    target = MedTentList.Count >= 1 ? MedTentList[0] : null;
+                    break;
+                case 2:
+                    target = MedTentList.Count >= 2 ? MedTentList[1] : null;
+                    break;
+                case 3:
+                    target = MedTentList.Count >= 3 ? MedTentList[2] : null;
+                    break;
+                    // Add additional cases if there are more than three Med Tents
+            }
+        }
 
-        // Draw a ray pointing at our target in
-        Debug.DrawRay(transform.position, newDirection, Color.red);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);
+        // Continue with the existing logic to rotate the arrow towards the target
+        if (target != null)
+        {
+            Vector3 targetDirection = target.transform.position - transform.position;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, 2 * Mathf.PI, 0.0f);
+            Debug.DrawRay(transform.position, newDirection, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
     }
 }
